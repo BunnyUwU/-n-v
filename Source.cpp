e982a01a8161914165ea614b48e615e0b1b84ca9
LolisHunter <47134305+LolisHunter@users.noreply.github.com>
2019-02-14 22:12:32 +0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+//
+//                                           BLA BLA BLA
+//																	# Need coffee . . .
+// 
+//                             .__(.)< (MEOW)
+//                             	\___)
+//							~~~~~~~~~~~~~~~~~~
+#include "meatball.h"
+
+void main()
+{
+	admin key;
+	key.load_student();
+	key.save_student();
+	_getch();
+}
